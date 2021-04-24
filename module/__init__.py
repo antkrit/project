fcd2ee23599cd4c0cec12cba75fda6d8de15d104
Krytskyi Anton <mujanjagusav@gmail.com>
2021-04-24 21:50:47 +0300

    logging/dotenv

--- module/__init__.py
+++ module/__init__.py
@@ -1,6 +1,8 @@
 """Initializes all dependencies and creates apps"""
 import os
+import logging
 from datetime import timedelta
+from logging.handlers import RotatingFileHandler, SMTPHandler
 from flask import Flask, session
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -32,6 +34,7 @@ class App:
         self.migration_folder = os.path.join('module', 'server', 'models', 'migrations')
         self.templates_folder = os.path.join('.', 'client', 'templates')
         self.static_folder = os.path.join('.', 'client', 'static')
+        self.logs_folder = os.path.join('module', 'logs')
 
         # App
         self._app = Flask(__name__, template_folder=self.templates_folder, static_folder=self.static_folder)
@@ -41,11 +44,13 @@ class App:
         App.migrate.init_app(self._app, App.db, directory=self.migration_folder)
         App.login_manager.init_app(self._app)
 
+        self.setup_logging()
+
         @self._app.before_request
         def before_request():
             # Updates the session time before each request to server. Sets it to 5 minutes (may be changed).
             session.permanent = True
-            self._app.permanent_session_lifetime = timedelta(minutes=2)
+            self._app.permanent_session_lifetime = timedelta(minutes=5)
 
     def register_blueprints(self, *args):
         """
@@ -58,7 +63,52 @@ class App:
             try:
                 self._app.register_blueprint(bp)
             except Exception as e:
-                print("An error occurred while registration blueprint - {}".format(e))
+                self._app.logger.error("An error occurred while registration blueprint - {}".format(e))
+
+    def setup_logging(self, logs_folder=None, log_level=logging.DEBUG):
+        """
+        Configures the app logging system.
+
+        :param logs_folder: folder where log files will be stored
+        :param log_level: logger level (debug, info, warning, error, exception, critical)
+        """
+
+        # Logging to the file
+        path_to_logs = logs_folder if logs_folder else self.logs_folder
+
+        if not os.path.exists(path_to_logs):
+            os.mkdir(path_to_logs)
+
+        file_handler = RotatingFileHandler(
+            os.path.join(path_to_logs, 'main.log'),
+            maxBytes=10240,
+            backupCount=5
+        )
+        file_handler.setFormatter(
+            logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+        )
+
+        self._app.logger.addHandler(file_handler)
+        self._app.logger.setLevel(log_level)
+
+        # Email notifications about failures
+        auth = secure = None
+
+        if self._app.config['MAIL_USERNAME'] and self._app.config['MAIL_PASSWORD']:
+            auth = (self._app.config['MAIL_USERNAME'], self._app.config['MAIL_PASSWORD'])
+        if self._app.config['MAIL_USE_TLS']:
+            secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost=(self._app.config['MAIL_SERVER'], self._app.config['MAIL_PORT']),
+            fromaddr='no-reply@cabinet.support',
+            toaddrs=self._app.config['ADMINS'],
+            subject='Cabinet Error',
+            credentials=auth,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        self._app.logger.addHandler(mail_handler)
 
     def get_flask_app(self):
         """Returns an object with Flask instance."""
@@ -89,6 +139,8 @@ class App:
                 # Add & save to the database
                 App.db.session.add(admin)
                 App.db.session.commit()
+
+        self._app.logger.info('Website startup')
 
         if name == '__main__':
             # Run the application with debug mode(not testing) if file was started directly ($ python file.py).
--- module/server/config.py
+++ module/server/config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
+
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, '.env'))
 
 
 class Config:
+    """Parent configuration class."""
     # WARNING: keep the secret key used in production secret
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
 
@@ -11,8 +15,18 @@ class Config:
         'sqlite:///' + os.path.join(BASEDIR, 'app.db')  # stores database file "app.db" by the ../module/server/ path
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # Mail
+    # By default is configured on the Python SMTP debugging server
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 8025)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or False
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # Who'll send emails
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['email@example.com']  # Who'll receive emails about failures
+
 
 class TestConfig(Config):
+    """Configurations for Testing, with a separate test database."""
     # If the application is tested - the database is created in the memory of the machine.
     # After working with it must be deleted (temp_db.session.remove(); temp_db.drop_all()).
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
